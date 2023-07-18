c858a12702c19ab7a0aef100704b683243e458b8
Chris-Carty <christopher.carty@tink.com>
2023-07-18 17:38:20 +0100

    update api endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://p2p-payments-server-etsuh.ondigitalocean.app",
 });
 
 export default api;
